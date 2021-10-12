647d1436c9520455ffb0573400dca1c2bcea953d
Jakub Skibiński <j.skibinski@teb-akademia.pl>
2021-10-12 20:11:55 +0200

    adding readme content

--- src/main/java/pages/MainPage.java
+++ src/main/java/pages/MainPage.java
@@ -1,8 +1,12 @@
 package pages;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
 
-public class MainPage {
+public class MainPage extends BasePage {
+
     public MainPage(WebDriver driver) {
+        super(driver);
+        PageFactory.initElements(driver, this);
     }
 }
