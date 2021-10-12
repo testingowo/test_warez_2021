ab03b6a109a0f51a97231986bd1fa9b88aedd533
Jakub Skibiński <j.skibinski@teb-akademia.pl>
2021-10-12 20:20:10 +0200

    modifying the testWarez class

--- src/test/java/testWarez.java
+++ src/test/java/testWarez.java
@@ -1,9 +1,11 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Platform;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import pages.MainPage;
@@ -22,15 +24,20 @@ public class testWarez {
 
     @BeforeEach
     public void navigateToMainPage() throws MalformedURLException {
-        desiredCapabilities = DesiredCapabilities.chrome();
-        desiredCapabilities.setCapability("--no-sandbox", true);
-        System.out.println("Desired capabilities"+ desiredCapabilities);
-        desiredCapabilities.setCapability("version", "");
-        desiredCapabilities.setPlatform(Platform.LINUX);
-        driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), desiredCapabilities);
-        driver.manage().window().maximize();
-        mainPage = new MainPage(driver);
+        //DOCKER SELENIUM GRID
+//        desiredCapabilities = DesiredCapabilities.chrome();
+//        desiredCapabilities.setCapability("--no-sandbox", true);
+//        System.out.println("Desired capabilities"+ desiredCapabilities);
+//        desiredCapabilities.setCapability("version", "");
+//        desiredCapabilities.setPlatform(Platform.LINUX);
+//        driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), desiredCapabilities);
+//        driver.manage().window().maximize();
+//        mainPage = new MainPage(driver);
 //        mainPage.navigateToPage(pageName);
+        //OLD
+        WebDriverManager.chromedriver().ignoreVersions("95", "94", "93");
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
     }
 
     @Test
